81059d40ba026097e580c124aceedcd074ce854a
张新强 <191988637@qq.com>
2016-06-29 22:47:11 +0800

    temptyview_lib -> temptyview

--- demo/src/main/java/com/barryzhang/temptyviewdemo/MainActivity.java
+++ demo/src/main/java/com/barryzhang/temptyviewdemo/MainActivity.java
@@ -1,38 +1,27 @@
 package com.barryzhang.temptyviewdemo;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.SparseArray;
-import android.view.View;
-import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.TableLayout;
+import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.barryzhang.temptyview.TEmptyView;
 import com.barryzhang.temptyview.TViewUtil;
 
-import java.io.InterruptedIOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
@@ -56,7 +45,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        TViewUtil.EmptyViewBuilder emptyViewConfig = TViewUtil.EmptyViewBuilder.getInstance(this)
+        TViewUtil.EmptyViewBuilder emptyViewConfig = TViewUtil.EmptyViewBuilder.getInstance(getApplicationContext())
                 .setShowText(true)
                 .setEmptyText("没有数据")
                 .setShowButton(false)
